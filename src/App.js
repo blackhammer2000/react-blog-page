5c128273ade0bec63aa572e88a3fb02c5de9168f
wawerusamuel <waweruzamuel@gmail.com>
2022-08-23 22:29:28 +0300

    added a footer component...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Blogs />
+      <Footer />
     </div>
   );
 }
