10320f6d97648a00e4c1df32cf72035e34513c42
wawerusamuel <waweruzamuel@gmail.com>
2022-08-23 19:13:00 +0300

    Initialize project using Create React App

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/bootstrap.min.css";
+import "./css/header.css";
+
+const Header = () => {
+  return (
+    <div className="container border text-center">
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
